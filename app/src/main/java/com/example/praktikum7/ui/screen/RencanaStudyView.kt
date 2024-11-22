efab10d67904896f24cac5beac9f81970e55f97f
Wiranto Akbar Fadlillah <alvinex1999@gmail.com>
2024-11-22 07:08:02 +0700

    menambah image

--- app/src/main/java/com/example/praktikum7/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikum7/ui/screen/RencanaStudyView.kt
@@ -1,10 +1,26 @@
 package com.example.praktikum7.ui.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.praktikum7.R
 import com.example.praktikum7.model.Mahasiswa
 
 @Composable
@@ -26,5 +42,22 @@ fun RencanaStudyView(
         mutableStateOf("")
     }
     var listData: MutableList<String> = mutableListOf(choosenDropdown, pilihanKelas)
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary))
+    ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp), verticalAlignment = Alignment.CenterVertically
+        ){
+            Image(painter = painterResource(id = R.drawable.umy), contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+                )
+            
+        }
+    }
 }
