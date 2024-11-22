d80844ebeaa88d7d07749f9f83fc0cca27822c75
Wiranto Akbar Fadlillah <alvinex1999@gmail.com>
2024-11-22 07:54:04 +0700

    menambah function untuk mata kuliah

--- app/src/main/java/com/example/praktikum7/ui/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/praktikum7/ui/viewmodel/RencanaStudyViewModel.kt
@@ -4,8 +4,14 @@ import com.example.praktikum7.model.RencanaStudy
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel {
     private val _krsState = MutableStateFlow(RencanaStudy())
     val krsStateUi: StateFlow<RencanaStudy> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String){
+        _krsState.update { stateMK -> stateMK.copy(mataKuliah = mkPilihan) }
+    }
+
 }
